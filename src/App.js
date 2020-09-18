2d45a6ca6b121e0e65de3530a6dbebf58ece5341
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-09-18 15:00:36 -0500

    Added AddWorkout component with corresponding styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Home } from './views/Home';
+import { AddWorkout } from './views/AddWorkout';
 import './sass/index.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route path="/addworkout" component={AddWorkout} />
       </div>
     </Router>
   );
