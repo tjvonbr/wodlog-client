a637688428ce6470c1b0fdc5c2cd5b723b2ff983
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-10-01 23:57:25 -0500

    Changed background color and shape of workout card

--- src/views/Workout.js
+++ src/views/Workout.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { WorkoutDate } from "../components/workout/workout-date";
 import { WorkoutDescription } from "../components/workout/workout-description";
+import { WorkoutNotes } from "../components/workout/workout-notes";
 
-function Workout() {
+function Workout(props) {
   const workout = window.localStorage.getItem("workout");
   const parsed = JSON.parse(workout);
 
@@ -10,8 +11,20 @@ function Workout() {
     <div className="workout-outer-wrapper">
       <div className="workout-inner-wrapper">
         <WorkoutDate data={parsed} />
-        <h3 className="workout-headers">Description</h3>
-        <WorkoutDescription data={parsed} />
+        <div>
+          <h3 className="workout-headers">Description</h3>
+          <WorkoutDescription data={parsed} />
+        </div>
+        <div>
+          <h3 className="workout-headers">Notes</h3>
+          <WorkoutNotes data={parsed} />
+        </div>
+        <button 
+          className="btn sm blue btm"
+          onClick={() => props.history.push("/results") }  
+        >
+          Close
+        </button>
       </div>
     </div>
   )
