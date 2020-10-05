d6b100cf74484120d1b4280d56a1acebb80de2f2
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-10-05 15:59:13 -0500

    Added helpful hints modal--still need to add animations

--- src/components/help-modal.js
+++ src/components/help-modal.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { HelpItem } from "./help-item";
+import { ImCircleUp } from "react-icons/im"
+import { searchHints } from "../data/search-hints";
 
 function HelpModal({ modal, hide }) {
   const modalState = modal ? "modal-wrapper--show" : "modal-wrapper--hide";
 
   return (
-    <div className={modalState} onClick={hide}>
-
+    <div className={modalState}>
+      {searchHints.map((hint, index) => (
+        <HelpItem hint={hint} key={index} />
+      ))}
+      <ImCircleUp 
+        className="icon collapse"
+        onClick={hide}  
+      />
     </div>
   )
 };
